e52b902f246fc87201f4793e968ec90a1ca995e8
Ryan Gruss <gruss.ryan@gmail.com>
2023-11-08 00:55:38 -0800

    add riker

--- api/function_app.py
+++ api/function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import logging
+from rikeripsum import rikeripsum
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -23,3 +24,23 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+@app.route(route="rikertext")
+def rikertext(req: func.HttpRequest) -> func.HttpResponse:
+    #logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+    returnText = rikeripsum.generate_paragraph()
+    #if name:
+    #    return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    #else:
+    #    return func.HttpResponse(
+    #         "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    return func.HttpResponse(returnText, status_code=200)
